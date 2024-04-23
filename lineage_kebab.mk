a4864502df22053dcdaa5c6984afe5e523e0b305
AnierinB <anierin@evolution-x.org>
2024-04-23 15:07:22 -0700

    kebab: Initialize for Evolution X 9.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- lineage_kebab.mk
+++ lineage_kebab.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from kebab device
 $(call inherit-product, device/oneplus/kebab/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common Evolution X stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_kebab
